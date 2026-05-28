911bfded2acd06a9dc00c62446e60805be3134e9
NitinKumarSamal-71 <nitinsamal4908@gmail.com>
2026-05-28 15:22:28 +0530

    Implement swap functionality in main function with third variable

---
+++ q12a.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var a = 10
+    var b = 20
+
+    println("Before Swapping:")
+    println("a = $a, b = $b")
+
+    val temp = a
+    a = b
+    b = temp
+
+    println("After Swapping:")
+    println("a = $a, b = $b")
+}
